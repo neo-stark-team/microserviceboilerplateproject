6df75e0262e608da4dbf82e7e0ac86f4f67885c3
Iamneo <techadmin@iamneo.ai>
2023-08-18 10:51:27 +0000

    Backup Commit

--- dotnetapp/Model/BookRepository.cs
+++ dotnetapp/Model/BookRepository.cs
@@ -7,6 +7,20 @@ namespace dotnetapp.Models
     public class BookRepository :IBookRepository  
     {  
         private List<Book> books = new ArrayList<Book>();  
-        
+
+        public List<Book> getAllBooks()
+        {
+            return books;
+        }
+        public Book AddBook(Book newBook)
+        {
+            Book book = new Book();
+            book.BookId(newBook.BookId());
+            book.BookName(newBook.BookName());
+            book.Category(newBook.Category());
+            book.Price(newBook.Price());
+            books.add(book);
+            return book;
+        }
     }
 } 
