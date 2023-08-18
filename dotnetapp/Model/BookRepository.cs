2b5c4f7ca0099cd4b897398a27eb8cfbc595aef4
Iamneo <techadmin@iamneo.ai>
2023-08-18 10:46:27 +0000

    Backup Commit

--- dotnetapp/Model/BookRepository.cs
+++ dotnetapp/Model/BookRepository.cs
@@ -6,50 +6,7 @@ namespace dotnetapp.Models
 {  
     public class BookRepository :IBookRepository  
     {  
-        private List<Book> books = new List<Book>();  
-        private int _nextId = 1;  
-        public BookRepository()  
-        {  
-            AddBook(new Book { Name = "Book1", Category = "Fiction", Price = 100});  
-            AddBook(new Book { Name = "Book2", Category = "Story", Price = 150 });  
-            AddBook(new Book { Name = "Book3", Category = "Fiction", Price = 200});  
-        }  
-        public IEnumerable<Book> GetAll()  
-        {  
-            return books;  
-        }  
-        public Book GetBook(int id)  
-        {  
-            return books.Find(p => p.Id == id);  
-        }  
-        public Book AddBook(Book item)  
-        {  
-            if (item == null)  
-            {  
-                throw new ArgumentNullException("item");  
-            }  
-            item.Id = _nextId++;  
-            books.AddBook(item);  
-            return item;  
-        }  
-        public void RemoveBook(int id)  
-        {  
-            books.RemoveAll(p => p.Id == id);  
-        }  
-        public bool UpdateBook(Book item)  
-        {  
-            if (item == null)  
-            {  
-                throw new ArgumentNullException("item");  
-            }  
-            int index = books.FindIndex(p => p.Id == item.Id);  
-            if (index == -1)  
-            {  
-                return false;  
-            }  
-            books.RemoveAt(index);  
-            books.AddBook(item);  
-            return true;  
-        }  
-    }  
+        private List<Book> books = new ArrayList<Book>();  
+        
+    }
 } 
