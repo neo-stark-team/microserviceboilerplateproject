7a16374e9b33b387aafcb461b54480b517ba5466
Iamneo <techadmin@iamneo.ai>
2023-09-04 18:42:12 +0000

    Backup Commit

--- dotnetappms1/TestMicroservice/UnitTest1.cs
+++ dotnetappms1/TestMicroservice/UnitTest1.cs
@@ -1,15 +1,30 @@
-namespace TestMicroservice;
+using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using dotnetappms1.Controllers;
 
-public class Tests
+namespace dotnetappms1.Tests
 {
-    [SetUp]
-    public void Setup()
+    [TestFixture]
+    public class StudentAdmissionControllerTests
     {
-    }
+        [Test]
+        public void Get_ReturnsListOfStudentAdmissionDetails()
+        {
+            // Arrange
+            var controller = new StudentAdmissionController();
 
-    [Test]
-    public void Test1()
-    {
-        Assert.Pass();
+            // Act
+            var result = controller.Get();
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOf<IEnumerable<StudentAdmissionDetailsModel>>(result);
+
+            var admissionList = (IEnumerable<StudentAdmissionDetailsModel>)result;
+            Assert.AreEqual(2, admissionList.Count());
+
+            // You can add more specific assertions if needed, e.g., check the properties of each student.
+        }
     }
 }
