43c9cb10b45d1bd9d586a3dae1246e10c6281baa
Iamneo <techadmin@iamneo.ai>
2023-09-04 18:37:12 +0000

    Backup Commit

---
+++ dotnetappms1/TestMicroservice/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace TestMicroservice;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
