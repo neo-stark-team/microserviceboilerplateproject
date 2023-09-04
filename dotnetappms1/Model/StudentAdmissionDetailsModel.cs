c778b1c7289e6b5fbecadffa19123003d33ff992
Iamneo <techadmin@iamneo.ai>
2023-09-04 18:27:12 +0000

    Backup Commit

---
+++ dotnetappms1/Model/StudentAdmissionDetailsModel.cs
@@ -0,0 +1,11 @@
+using System;
+namespace dotnetappms1
+{
+    public class StudentAdmissionDetailsModel
+    {
+        public int StudentID { get; set; }
+        public string StudentName { get; set; }
+        public string StudentClass { get; set; }
+        public DateTime DateofJoining { get; set; }
+    }
+}
---
+++ dotnetappms1/msdotnetapp2/Model/StudentAttendanceDetailsModel.cs
@@ -0,0 +1,10 @@
+using System;
+namespace msdotnetapp2
+{
+    public class StudentAttendanceDetailsModel
+    {
+        public int StudentID { get; set; }
+        public string StudentName { get; set; }
+        public double AttendencePercentage { get; set; }
+    }
+}
