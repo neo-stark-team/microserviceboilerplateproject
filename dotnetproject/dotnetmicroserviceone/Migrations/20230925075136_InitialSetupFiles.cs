062dd3f1e120b108e2faa5dcea3502231c4c3243
Iamneo <techadmin@iamneo.ai>
2023-09-25 07:55:08 +0000

    Backup Commit

---
+++ dotnetproject/dotnetmicroserviceone/Migrations/20230925075136_InitialSetupFiles.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace dotnetmicroserviceone.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialSetupFiles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Songs",
+                columns: table => new
+                {
+                    SongID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SongName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ReleaseYear = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SingerName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Songs", x => x.SongID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Songs");
+        }
+    }
+}
