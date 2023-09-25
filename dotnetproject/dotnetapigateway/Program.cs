541e21c7cebcc2fdaecd08d3ae7d1b2ee6898a44
Iamneo <techadmin@iamneo.ai>
2023-09-25 07:59:58 +0000

    Backup Commit

--- dotnetproject/dotnetapigateway/Program.cs
+++ dotnetproject/dotnetapigateway/Program.cs
@@ -1,6 +1,33 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
+    .AddJsonFile("Ocelot.json", optional: false, reloadOnChange: true)
+    .AddEnvironmentVariables();
+
+builder.Services.AddControllers();
+builder.Services.AddOcelot(builder.Configuration);
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.UseOcelot().Wait();
 
 app.Run();
