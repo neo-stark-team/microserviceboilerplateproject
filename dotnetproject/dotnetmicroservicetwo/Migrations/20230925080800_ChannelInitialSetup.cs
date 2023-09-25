9a98c5f8fdbc8f387557e1dcac0d0cea5bd044d1
Iamneo <techadmin@iamneo.ai>
2023-09-25 08:10:08 +0000

    Backup Commit

---
+++ dotnetproject/dotnetmicroservicetwo/Migrations/20230925080800_ChannelInitialSetup.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace dotnetmicroservicetwo.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChannelInitialSetup : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Channels",
+                columns: table => new
+                {
+                    ChannelID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ChannelName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    POCName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CommercialPerAd = table.Column<decimal>(type: "decimal(18,2)", nullable: true),
+                    MailID = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ContactNumber = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Channels", x => x.ChannelID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Channels");
+        }
+    }
+}
