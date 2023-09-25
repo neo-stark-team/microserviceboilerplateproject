cbd5560c6cc6c120e0f43a2a6fd28afd49e9895c
Ponmalar Kathiravan <ponmalar@iamneo.ai>
2023-09-25 08:29:04 +0000

    Dotnet Project

---
+++ dotnetproject/TestProject/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace TestProject;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+        //git remote add origin https://github.com/neo-stark-team/microserviceboilerplateproject.git
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
